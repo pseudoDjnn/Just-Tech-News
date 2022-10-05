bfdcdb1645712a2a438fe31a2e5f667e6c054b1b
p5eudoDjnn <alexander.havers@gmail.com>
2022-10-05 13:32:59 -0400

    could be a bad commit check

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -47,11 +47,37 @@ router.post("/", (req, res) => {
     });
 });
 
+router.post("/login", (req, res) => {
+  // Query operation
+  // expects {email: 'lernantino@gmail.com', password: 'password1234'}
+  User.findOne({
+    where: {
+      email: req.body.email,
+    },
+  }).then((dbUserData) => {
+    if (!dbUserData) {
+      res.status(400).json({ message: "No user with that email address!" });
+      return;
+    }
+    const validPassword = dbUserData.checkPassword(req.body.password);
+
+    if (!validPassword) {
+      res.status(400).json({ message: "Invalid password!" });
+      return;
+    }
+
+    res.json({ user: dbUserData, message: "You are logged in." });
+
+    // Verify user
+  });
+});
+
 router.put("/:id", (req, res) => {
   // expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
 
   // pass in req.body instead to only update what's passed through
   User.update(req.body, {
+    individualHooks: true,
     where: {
       id: req.params.id,
     },
